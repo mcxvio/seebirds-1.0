80c84001dd8f75b0b975f25f380259393bc3cd51
mrhapi <_mhunt@outlook.com>
2016-02-20 21:58:52 -0500

    Copyright 2016 updates, include py and yaml files
    Updates for 2016, include yaml and py files to show Search link on bare
    url.

---
+++ main.py
@@ -0,0 +1,10 @@
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write('<a href="index.html">Search</a>')
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
