dd680c5819400c54ccd9c43233c12977bba81df5
mrhapi <_mhunt@outlook.com>
2016-05-13 21:38:04 -0400

    webapp2 & scripts link updates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('<a href="index.html" class="centerbox">Search</a>')
+        self.response.write('<html><head></head><body><div style="text-align: center; padding-left: 50px; padding-top: 50px; font-size: 1.75em;"><p>Welcome to Seebirds:</p><a href="index.html">Search</a></div></body></html>')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
